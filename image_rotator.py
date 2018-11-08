a96566258b392fdc53f51a888f5df4ca8e5b4054
isaacolson <olsonites@gmail.com>
2018-11-08 15:47:04 -0600

    Update image_rotator.py

--- image_rotator.py
+++ image_rotator.py
@@ -1,22 +1,41 @@
+#Import modules
 import cv2
 import numpy as np
 
+#Define varibles
+angle = 12.7
+
+#Define function
+#From https://stackoverflow.com/a/33247373
+def rotateAndScale(img, scaleFactor, degreesCCW):
+    (oldY,oldX) = img.shape #note: numpy uses (y,x) convention but most OpenCV functions use (x,y)
+    M = cv2.getRotationMatrix2D(center=(oldX/2,oldY/2), angle=degreesCCW, scale=scaleFactor) #rotate about center of image.
+
+    #choose a new image size.
+    newX,newY = oldX*scaleFactor,oldY*scaleFactor
+    #include this if you want to prevent corners being cut off
+    r = np.deg2rad(degreesCCW)
+    newX,newY = (abs(np.sin(r)*newY) + abs(np.cos(r)*newX),abs(np.sin(r)*newX) + abs(np.cos(r)*newY))
+
+    #the warpAffine function call, below, basically works like this:
+    # 1. apply the M transformation on each pixel of the original image
+    # 2. save everything that falls within the upper-left "dsize" portion of the resulting image.
+
+    #So I will find the translation that moves the result to the center of that region.
+    (tx,ty) = ((newX-oldX)/2,(newY-oldY)/2)
+    M[0,2] += tx #third column of matrix holds translation, which takes effect after rotation.
+    M[1,2] += ty
+
+    rotatedImg = cv2.warpAffine(img, M, dsize=(int(newX),int(newY)))
+    return rotatedImg
+
+#Load image
 img = cv2.imread('rotateInitial.png',0)
-rows,cols = img.shape
+#Call Function
+rot = rotateAndScale(img, 1.0, angle)
+#Display rotated image
+cv2.imshow('image', rot)
 
-M = cv2.getRotationMatrix2D((cols/2,rows/2),12.7,1)
-dst = cv2.warpAffine(img,M,(cols,rows))
-
-cv2.imshow('img',dst)
+#Closed window
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
-angle = 45
-width = int((np.cos(np.deg2rad(angle))*640)+(np.cos(np.deg2rad(90-angle))*480))
-height = int((np.sin(np.deg2rad(angle))*640)+(np.sin(np.deg2rad(90-angle))*480))
-
-cv2.namedWindow('image', cv2.WINDOW_NORMAL)
-res = cv2.resizeWindow( dst, (width,height))
-
-
-cv2.imshow('image', res)
